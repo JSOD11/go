671405a53ff600f8d1c1f2cee44468ad3d32277e
Justin O'Dwyer <justin.odwyer@datadoghq.com>
2025-08-12 20:17:39 -0400

    Job queue and worker pool working with randomized worker speeds.

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sort"
+	"sync"
 	"time"
 )
 
 var (
-	numJobs    = 100
-	numWorkers = 5
+	numJobs    = 100000
+	numWorkers = 10
+	speedLimit = 1000
+	wg         sync.WaitGroup
 )
 
 func main() {
@@ -17,69 +21,94 @@ func main() {
 	jobQueue := make(chan int)
 	results := make(chan int)
 
-	go startWorkerPool(jobQueue, results)
-	time.Sleep(1 * time.Second)
+	workers := startWorkerPool(jobQueue, results)
+	time.Sleep(time.Second / 2)
+
 	go beginJobStream(jobQueue)
-	readResults(results)
+
+	wg.Add(1)
+	go readResults(results)
+
+	wg.Wait()
+
+	summarizePerformance(workers)
 }
 
 func startWorkerPool(jobQueue <-chan int, results chan<- int) []*worker {
-	fmt.Printf("\nStarting worker pool.\n")
+	fmt.Printf("Starting worker pool.\n")
 	var workers []*worker
 	for i := range numWorkers {
-		speed := int(math.Max(float64(1), float64(rand.Intn(5))))
+		speed := int(math.Max(float64(1), float64(rand.Intn(speedLimit))))
 		w := newWorker(uint(i), speed, jobQueue, results, processFn)
+
+		wg.Add(1)
 		go w.start()
+
 		workers = append(workers, w)
 	}
 	return workers
 }
 
 type worker struct {
-	id             uint
-	tasksCompleted uint
-	speed          int
-	inputStream    <-chan int
-	outputStream   chan<- int
-	processFunc    func(int, int) int
+	id                uint
+	numTasksCompleted uint
+	speed             int
+	inputStream       <-chan int
+	outputStream      chan<- int
+	processFunc       func(int, int) int
 }
 
-func newWorker(id uint, speed int, inputStream <-chan int, outputStream chan<- int, processFunc func(int, int) int) *worker {
+func newWorker(
+	id uint,
+	speed int,
+	inputStream <-chan int,
+	outputStream chan<- int,
+	processFunc func(int, int) int) *worker {
 	return &worker{
-		id:             id,
-		tasksCompleted: 0,
-		speed:          speed,
-		inputStream:    inputStream,
-		outputStream:   outputStream,
-		processFunc:    processFunc,
+		id:                id,
+		numTasksCompleted: 0,
+		speed:             speed,
+		inputStream:       inputStream,
+		outputStream:      outputStream,
+		processFunc:       processFunc,
 	}
 }
 
 func (w *worker) start() {
-	fmt.Printf("\nStarting worker %v with speed %v.\n", w.id, w.speed)
+	defer wg.Done()
 	for input := range w.inputStream {
 		output := w.processFunc(input, w.speed)
+		w.numTasksCompleted++
 		w.outputStream <- output
-		fmt.Printf("Worker %v processed an item: %v.\n", w.id, input)
 	}
 }
 
 func processFn(input, speed int) int {
 	time.Sleep(time.Second / time.Duration(speed*10))
-	return input * 2
+	return input
 }
 
 func beginJobStream(jobQueue chan<- int) {
-	fmt.Printf("\nStarting job stream.\n\n")
+	fmt.Printf("Starting job stream.\n")
 	for i := range numJobs {
 		jobQueue <- i
 	}
+	close(jobQueue)
 }
 
 func readResults(results <-chan int) {
-	for i := range numJobs {
-		result := <-results
-		fmt.Printf("Result %v: %v\n", i, result)
+	defer wg.Done()
+	for range numJobs {
+		<-results
 	}
-	fmt.Printf("\nAll jobs complete.\n")
+}
+
+func summarizePerformance(workers []*worker) {
+	fmt.Printf("\n———————— Performance Summary ———————\n")
+	sort.Slice(workers, func(i, j int) bool {
+		return workers[i].speed > workers[j].speed
+	})
+	for _, w := range workers {
+		fmt.Printf("\nWorker %v\n——————————————————\nSpeed: %v\nNum tasks completed: %v\n", w.id, w.speed, w.numTasksCompleted)
+	}
 }
